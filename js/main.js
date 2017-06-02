2a74fc49763e189a1df787e2980f01337d53c679
jsap1904 <sapinoso.j@gmail.com>
2017-06-01 19:12:40 -0700

    update

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ function bookSearch() {
 	var search = document.getElementById('search').value
 	document.getElementById("results").innerHTML = ""
 	console.log('search')
-...
+
 
 $.ajax ({
-	url: "https://www.googleapis.com/books/v1/volumes?q=" + 
+	url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+	dataType: "json" ,
 
 	success: function(data) {
-		var results = document.getElementById.("results")
+		var results = document.getElementById("results")
 		for(i=0; i<data.items.length; i++){
 			results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
 		}
