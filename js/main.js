68b86a5073b24776d2f6a304694fe2148c8da83a
jsap1904 <sapinoso.j@gmail.com>
2017-06-07 21:30:45 -0700

    update

--- js/main.js
+++ js/main.js
@@ -7,11 +7,59 @@ function bookSearch() {
 $.ajax ({
 	url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
 	dataType: "json" ,
+	maxResults: "20",
 
 	success: function(data) {
 		var results = document.getElementById("results")
 		for(i=0; i<data.items.length; i++){
-			results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+			var div = document.createElement("div")
+			div.className = "book"
+			results.appendChild(div)
+
+			var text = document.createElement("div")
+			text.className = "text"
+
+			var image = document.createElement("img")
+			img.src += books.items[i].volumeInfo.imageLinks.thumbnail
+			image.className = "thumbnail"
+			div.appendChild(image)
+			var titleText = document.createTextNode( books.items[i].volumeInfo.title)
+			var title = document.createElement("h3")
+			title.appendChild(titleText)
+			text.appendChild(title)
+			div.appendChild(text)
+
+			var authorsText = document.createTextNode(books.items[i].volumeInfo.authors)
+			var authors = document.createElement("h4")
+			authors.appendChild(authorsText)
+			text.appendChild(authors)
+
+			var descriptionText = document.createTextNode(books.items[i].searchInfo.textSnippet)
+			var description = document.createElement("h5") 
+			description.appendChild(descriptionText)
+			text.appendChild(description)
+
+			var btn = document.createElement("button")
+			var textLink = document.createElement("a")
+			var moreInfo = document.createTextNode("more info")
+			btn.className = "moreInfo"
+			textLink.href += data.items[i].saleInfo.buyLink
+
+			if (data.items[i].saleInfo.buyLink === "undefined") {
+				text.href += "https://www.amazon.com/b/ref=sr_aj?node=283155&ajr=0"
+				console.log("works")
+			}
+			textLink.target += " _blank"
+			textLink.appendChild(moreInfo)
+			btn.appendChild(textLink)
+			div.appendChild(btn)
+			console.log(books.items[i])
+
+
+
+
+
+			//results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
 		}
 		
 	},
@@ -21,5 +69,10 @@ $.ajax ({
 });
 
 }
+$("#search").keyup(function(event){
+	if(event.keyCode ==13) {
+		$("#button").click();
+	}
+});
 
 document.getElementById('button').addEventListener('click', bookSearch, false)
